7bef9db1936e6b7a72b831789016745efef6e6db
pranathikumari <pranathikumarin@gmail.com>
2026-08-25 09:26:06 +0530

    initial commit

---
+++ week5/dynamic_app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+
+const todoArr = [{
+    id: 1,
+    title: "learn express",
+    tags: ['nodejs', "express"],
+    status: { "pending": true, statusid: 1 }
+}, {
+    id: 2,
+    title: "learn express",
+    tags: ['nodejs', "express"],
+    status: { "pending": true, statusid: 1 }
+}];
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the Express server!");
+});
+
+app.get("/about", (req, res) => {
+    res.send("This is the about page.");
+});
+
+app.get("/todos/:id", (req, res) => {
+    const paramsId = parseInt(req.params.id);
+    const todo = todoArr.find((todo) => todo.id === paramsId);
+    res.send(todo);
+    console.log(todo);
+});
+
+
+/* This route is to get a todo item by its id and status.*/
+app.get("/todos/:id/status/:status", (req, res) => {
+    const paramsId = parseInt(req.params.id);
+    const paramsStatus = req.params.status;
+    const todo = todoArr.find((todo) => todo.id === paramsId && todo.status === paramsStatus);
+    res.send(todo);
+    console.log(todo);
+});
+
+
+/* This route is to get status of a todo item by its id.*/
+app.get("/todos/:id/status", (req, res) => {
+    const paramsId = parseInt(req.params.id);
+    const todo = todoArr.find((todo) => todo.id === paramsId);
+    res.send(todo.status);
+    console.log(todo.status);
+});
+
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
